e1f6bf42cf11eb4d4d24a68311b0b920bbf4e329
Justin Putz <jrputz1@gmail.com>
2019-06-13 11:23:47 -0500

    Added search functionallity and styling

--- src/CardList.js
+++ src/CardList.js
@@ -6,16 +6,16 @@ const CardList = ({ robots }) => {
     <div>
       {
         robots.map((user, i) => {
-        return (
-          <Card
-            key={i}
-            id={robots[i].id}
-            name={robots[i].name}
-            email={robots[i].email}
-          />
-        );
-      })
-    }
+          return (
+            <Card
+              key={i}
+              id={robots[i].id}
+              name={robots[i].name}
+              email={robots[i].email}
+            />
+          );
+        })
+      }
     </div>
   );
 }
